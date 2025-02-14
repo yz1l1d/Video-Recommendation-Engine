96ad284dfb0502cdedd291e1f80c2821b30d0f6f
yz1l1d <yubarajsingha2808@gmail.com>
2025-02-15 00:54:19 +0530

    Postman

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Video Recommendation Engine!"}
+
+@app.get("/feed")
+def get_feed(username: str):
+    return {"message": f"Feed for {username}"}
